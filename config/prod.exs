f28682bc1e7eed4cdc9b9d876b0ab8a2dbbf7120
Roope Kangas <muhmi@iki.fi>
2015-10-27 21:02:39 +0200

    Trying to follow https://github.com/phoenixframework/phoenix_guides/blob/master/deployment/E_heroku.md

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :crux, Crux.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "arcane-woodland-8154.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Ecto
+config :crux, Crux.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
 
 # ## SSL Support
 #
@@ -58,4 +67,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+
