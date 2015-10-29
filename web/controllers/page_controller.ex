4626aca614968bc223af23c9cd50b3bf10f2d4d3
Roope Kangas <muhmi@iki.fi>
2015-10-29 14:49:45 +0200

    redirect to posts

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Crux.PageController do
   use Crux.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    redirect conn, to: post_path(conn, :index)
   end
 end
